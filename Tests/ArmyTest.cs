674d7de6ca43170d2e12320147baa3092b13cc05
Vassil Mladenov <vmladenov@icloud.com>
2017-02-17 11:50:20 -0600

    added MSTest framework and Army test, updated travis dotnet core version

---
+++ Tests/ArmyTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class ArmyTest
+{
+    [TestMethod]
+    public void Construction()
+    {
+        var a = new Army(new Pos(0, 3), 100);
+        Assert.AreEqual(100, a.Health);
+        Assert.AreEqual(0, a.Position.X);
+        Assert.AreEqual(3, a.Position.Y);
+    }
+}
