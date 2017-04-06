5919d29cedc24a7cb15eb53ba4c6dd56260cdc9e
Vassil Mladenov <vmladenov@icloud.com>
2017-04-05 23:15:10 -0500

    Migrated to release .NET Core

--- Tests/WorldTest.cs
+++ Tests/WorldTest.cs
@@ -6,7 +6,7 @@ public class WorldTest
     [TestMethod]
     public void TestParseWorldCSV()
     {
-        World world = new World("maps.csv");
+        World world = new World("./../../../maps.csv");
         Province prov = world.GetProvinceAt(new Pos(29, 10));
         Assert.IsNotNull(prov.City);
         Assert.AreEqual("Puerto Natales", prov.City.Name);
